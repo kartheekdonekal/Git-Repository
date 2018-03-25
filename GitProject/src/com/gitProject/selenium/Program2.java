81c1150f3ffba6744bc69bc93d5555192ba4425e
Kartheek Chowdary <Chowdarykarthik425@gmail.com>
2018-03-25 18:02:59 +0530

    Commit to Masrter
    Commit

---
+++ GitProject/src/com/gitProject/selenium/Program2.java
@@ -0,0 +1,11 @@
+package com.gitProject.selenium;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		System.out.println("I am from Bellary");
+	    System.out.println("I am Learning Git");
+
+	}
+
+}
